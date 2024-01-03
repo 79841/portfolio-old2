4d46ccee05800f9c915e7bc039b6e0ffc72acfe6
LEtMeDEv <79841@naver.com>
2024-01-03 20:31:21 +0900

    add increase-right animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,15 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "increase-right": {
+          from: { width: "0" },
+          to: { width: "100%" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "increase-right": "increase-right 0.5s ease-out",
       },
     },
   },
