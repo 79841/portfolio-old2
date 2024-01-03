79c70c5d36781c708d205a676ff2a55e58667e47
LEtMeDEv <79841@naver.com>
2024-01-03 20:32:55 +0900

    fix: Navigator style changed

--- src/app/_components/navigation/Navigator.tsx
+++ src/app/_components/navigation/Navigator.tsx
@@ -1,10 +1,9 @@
 import { navigationLinks, siteMetadata } from "@/data";
-import Link from "next/link";
 import { NavigationLink } from "./NavigationLink";
 
 export const Navigator = () => {
   return (
-    <nav className="flex flex-col relative top-8 gap-4 w-fit">
+    <nav className="flex w-fit flex-col gap-2">
       {Object.values(navigationLinks).map((navInfo) => (
         <NavigationLink key={navInfo.name} {...navInfo} />
       ))}
