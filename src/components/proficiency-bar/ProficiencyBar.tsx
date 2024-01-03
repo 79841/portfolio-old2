8a184192b07b4c563afdec24b6d151b22a3870b6
LEtMeDEv <79841@naver.com>
2024-01-03 22:03:23 +0900

    feat: proficiency bar

---
+++ src/components/proficiency-bar/ProficiencyBar.tsx
@@ -0,0 +1,23 @@
+type TProficiencyBarProps = {
+  name: string;
+  proficiency: number;
+};
+export const ProficiencyBar = ({ name, proficiency }: TProficiencyBarProps) => {
+  return (
+    <div>
+      <div className="flex justify-end text-xs">proficiency</div>
+      <div className="relative w-32">
+        <div className="h-1 w-full rounded-full border border-black bg-transparent"></div>
+        <div
+          className={"absolute left-0 top-0"}
+          style={{ width: `${proficiency}%` }}
+        >
+          <div
+            key={name}
+            className={"animate-increase-right h-1 rounded-full bg-primary"}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
