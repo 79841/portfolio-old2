25b1f732ade2035c7cf3248cd5156dc72769364c
LEtMeDEv <79841@naver.com>
2024-01-05 21:54:58 +0900

    feat: add types

---
+++ src/types/Experience.ts
@@ -0,0 +1,6 @@
+export type TExperienceData = {
+  title: string;
+  period: string;
+  image: string;
+  content?: string;
+};
---
+++ src/types/Project.ts
@@ -0,0 +1,10 @@
+export type TProjectData = {
+  name: string;
+  description: string;
+  content: string;
+  techStack: string[];
+  date: string;
+  images?: string[];
+  demo?: string;
+  github?: string[];
+};
