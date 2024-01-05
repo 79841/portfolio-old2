36505e2083de886bd9a345182486dc059125957b
LEtMeDEv <79841@naver.com>
2024-01-05 21:46:01 +0900

    fix: add selected mark

--- src/app/_components/navigation/NavigationLink.tsx
+++ src/app/_components/navigation/NavigationLink.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { SelectedMark } from "@/components/selected-mark";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 type TNavigationLinkProps = {
@@ -8,9 +10,12 @@ type TNavigationLinkProps = {
 };
 export const NavigationLink = ({ name, path }: TNavigationLinkProps) => {
   const pathname = usePathname();
+  const isSelected = pathname === path;
   return (
     <Link href={path} className={"w-fit"}>
-      <span className={`${pathname === path && "font-semibold"}`}>{name}</span>
+      <SelectedMark isSelected={isSelected}>
+        <span className="text-sm hover:font-semibold">{name}</span>
+      </SelectedMark>
     </Link>
   );
 };
