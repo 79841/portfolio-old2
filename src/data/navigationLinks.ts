35e947d3b15470c88471454958f3705d7a6cdfd1
LEtMeDEv <79841@naver.com>
2024-01-06 23:46:30 +0900

    update nav links

--- src/data/navigationLinks.ts
+++ src/data/navigationLinks.ts
@@ -1,6 +1,6 @@
 export const navigationLinks = {
   home: { name: "Home", path: "/" },
   skills: { name: "Skills", path: "/skills" },
-  experiences: { name: "experiences", path: "/experiences" },
-  projects: { name: "projects", path: "/projects" },
+  experiences: { name: "Experiences", path: "/experiences" },
+  projects: { name: "Projects", path: "/projects" },
 };
