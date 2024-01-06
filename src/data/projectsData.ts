a2a2f3e053748dd7172a2e6d4987be5004455eb4
LEtMeDEv <79841@naver.com>
2024-01-06 23:46:40 +0900

    update project data

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,17 +1,19 @@
 import { TProjectData } from "@/types/Project";
 
-export const projectsData: TProjectData[] = [
-  {
+export const projectsData: Record<string, TProjectData> = {
+  securityGym: {
     name: "Security Gym",
-    description:
-      "침해사고가 발생했을 시 즉각적인 대응을 위한 훈련 시스템입니다.",
+    link: "/projects/security-gym",
+    description: "침해사고가 발생했을 시 즉각적인 대응을 위한 훈련 시스템",
     content:
       "기업에 침해사고 발생시 즉각적인 대응(Incident Response)을 위한 시스템입니다. BoB에서 진행한 5인 프로젝트로 시스템 구축과 포렌식을 맡아서 했습니다. 처음 진행한 협업 프로젝트이기에 의미가 큽니다. PHP와 Flask를 이용하여 서버를 개발했고 Docker와 VMware로 가상환경을 구현했습니다.",
-    techStack: ["PHP", "Flask", "Mysql"],
+    images: [],
+    techStack: ["Python", "PHP", "Flask", "Mysql", "Docker", "VMware"],
     date: "2017.09. ~ 2017.12.",
   },
-  {
-    name: "추신사",
+  chusinsa: {
+    name: "Chusinsa",
+    link: "/projects/chusinsa",
     description:
       "사용자의 성별, 키, 몸무게 정보를 이용하여 의류를 추천하는 웹 애플리케이션입니다.",
     content:
@@ -21,8 +23,9 @@ export const projectsData: TProjectData[] = [
     images: ["/images/projects/chusinsa/chusinsa.avif"],
     github: ["https://github.com/79841/chusinsa-front"],
   },
-  {
-    name: "렛츠듀",
+  letsdo: {
+    name: "Let'sdo",
+    link: "/projects/letsdo",
     description: "척수 장애인을 위한 건강관리 앱 입니다.",
     content:
       "척수 장애인들에게 건강 관리를 위한 프로젝트를 제공하고 간편하게 상담할 수 있는 모바일 앱입니다. 사용자용과 관리자용으로 두가지 앱을 개발하고 백엔드까지 개발한 1인 프로젝트입니다. 사용자들의 과제 수행도를 확인하기 위한 대시보드와 상담을 위한 실시간 채팅 기능을 만들었습니다. 처음으로 진행한 플러터 프로젝트이면서 웹 소켓을 이용한 채팅 프로그램에 깊은 이해를 갖게 되어 의미가 있습니다.",
@@ -47,8 +50,9 @@ export const projectsData: TProjectData[] = [
       "https://github.com/79841/letsdo-back",
     ],
   },
-  {
+  dreamShare: {
     name: "Dream Share",
+    link: "/projects/dream-share",
     description: "서로의 꿈을 공유하는 앱입니다.",
     content:
       "꿈 일기를 작성하고 공유하는 앱입니다. 현재 개발 중인 3인 프로젝트로 웹과 앱 개발을 동시에 진행하고 있습니다. 프론트엔드와 앱 개발을 맡고 있으며 백엔드는 코드리뷰로 참여하고 있고 Jira와 Slack을 통해 협업하고 있으면 Agile 방법론으로 개발하고 있습니다. 프로젝트 초기부터 체계적으로 설계한 프로젝트이고 Slack과 코드리뷰를 통해 적극적인 협업 중인 프로젝트이기에 의미가 큽니다. 또한 카카오와 구글 등의 소셜로그인을 구현하면서 OAuth와 OIDC에 대한 개념을 다질 수 있었습니다.",
@@ -60,11 +64,13 @@ export const projectsData: TProjectData[] = [
       "Spring Boot",
       "Mysql",
     ],
+    images: [],
     date: "2023.10. ~ 진행중",
     github: ["https://github.com/Dream-share"],
   },
-  {
+  portfolio: {
     name: "Portfolio",
+    link: "/projects/portfolio",
     description: "포트폴리오 사이트",
     content:
       "직접 개발한 포트폴리오 사이트 입니다. Nextjs14 app router과 shacn을 이용하여 개발하였습니다. shadcn을 처음 사용한 프로젝트이면서 opengraph에 대해 이해할 수 있었던 의미있는 프로젝트입니다. 깃허브 페이지로 배포하고 있고 지속적으로 기능을 추가할 계획입니다.",
@@ -73,4 +79,4 @@ export const projectsData: TProjectData[] = [
     images: ["/images/projects/portfolio/portfolio-dark.avif"],
     github: ["https://github.com/79841/portfolio"],
   },
-];
+};
