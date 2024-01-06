1f49a236b9c374af27f18de9378ac36dcc99caed
LEtMeDEv <79841@naver.com>
2024-01-06 23:45:48 +0900

    update selected mark

--- src/components/selected-mark/SelectedMark.tsx
+++ src/components/selected-mark/SelectedMark.tsx
@@ -4,7 +4,7 @@ import React, { PropsWithChildren } from "react";
 type TSelectedMark = { isSelected: boolean } & PropsWithChildren;
 export const SelectedMark = ({ isSelected, children }: TSelectedMark) => {
   return (
-    <div className="flex">
+    <div className="flex cursor-pointer">
       {isSelected && <div>*</div>}
       <div className={cn(isSelected && "font-semibold")}>{children}</div>
     </div>
