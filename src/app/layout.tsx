6e560e6b0c80c2456fb11ea20a5cbf51d5ce4613
LEtMeDEv <79841@naver.com>
2024-01-08 07:51:54 +0900

    refactor layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Navigator } from "./_components/navigation";
-import { Header } from "./_components/header";
 import { ThemeProvider } from "./_components/theme-mode/ThemeProvider";
 import { siteMetadata } from "@/data";
 import { Roboto_Mono, Ubuntu } from "next/font/google";
+import { MainLayout } from "./_components/layout";
 
 const ubuntu = Ubuntu({
   display: "swap",
@@ -41,19 +40,7 @@ export default function RootLayout({
     <html lang={siteMetadata.language}>
       <body className={`${ubuntu.className} ${nanum_pen_script.className}`}>
         <ThemeProvider attribute="class" enableSystem disableTransitionOnChange>
-          <div className="h-screen w-screen p-4 lg:p-8">
-            <div className="relative flex h-full w-full flex-col overflow-scroll border border-primary p-4 scrollbar-hide lg:flex-row lg:p-8">
-              <div className="flex w-full flex-row justify-between lg:w-fit lg:flex-col lg:justify-start">
-                <Header />
-                <Navigator />
-              </div>
-              <div className="flex flex-[1] items-start justify-end lg:items-end">
-                <div className="flex h-full w-full items-end justify-end">
-                  {children}
-                </div>
-              </div>
-            </div>
-          </div>
+          <MainLayout>{children}</MainLayout>
         </ThemeProvider>
       </body>
     </html>
