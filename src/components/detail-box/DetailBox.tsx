b51dd8420e8a9c899df0a4ad0addf8fd38510808
LEtMeDEv <79841@naver.com>
2024-01-08 11:55:48 +0900

    change padding

--- src/components/detail-box/DetailBox.tsx
+++ src/components/detail-box/DetailBox.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from "react";
 type TDetailBoxProps = PropsWithChildren;
 export const DetailBox = ({ children }: TDetailBoxProps) => {
   return (
-    <div className="static left-[50%] top-[50%] lg:absolute lg:w-96 lg:-translate-x-[50%] lg:-translate-y-[50%]">
+    <div className="static left-[50%] top-[50%] py-32 lg:absolute lg:w-96 lg:-translate-x-[50%] lg:-translate-y-[50%] lg:pt-0">
       <div className="flex flex-col  items-center justify-center gap-2 lg:h-60">
         {children}
       </div>
