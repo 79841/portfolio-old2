176431bd070cdd6669781cc30bf76a19112d1568
LEtMeDEv <79841@naver.com>
2024-01-09 09:34:53 +0900

    add blur event

--- src/app/_components/navigation/NavigationLink.tsx
+++ src/app/_components/navigation/NavigationLink.tsx
@@ -1,26 +1,24 @@
 "use client";
 
 import { SelectedMark } from "@/components/selected-mark";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { MouseEventHandler, memo } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { memo } from "react";
 type TNavigationLinkProps = {
   name: string;
   path: string;
-  onClick: MouseEventHandler<HTMLAnchorElement>;
 };
 export const NavigationLink = memo(function NavigationLink({
   name,
   path,
-  onClick,
 }: TNavigationLinkProps) {
   const pathname = usePathname();
+  const router = useRouter();
   const isSelected = pathname.split("/").at(1) === path.slice(1);
   return (
-    <Link href={path} className={"w-fit"} onClick={onClick}>
+    <div className={"w-fit"} onMouseDown={() => router.push(path)}>
       <SelectedMark isSelected={isSelected}>
         <span className="text-sm hover:font-semibold">{name}</span>
       </SelectedMark>
-    </Link>
+    </div>
   );
 });
--- src/app/_components/navigation/Navigator.tsx
+++ src/app/_components/navigation/Navigator.tsx
@@ -2,7 +2,7 @@
 import { navigationLinks } from "@/data";
 import { NavigationLink } from "./NavigationLink";
 import { DotIcon } from "lucide-react";
-import { MouseEventHandler, useState } from "react";
+import { FocusEventHandler, MouseEventHandler, useState } from "react";
 import { cn } from "@/lib/utils";
 
 export const Navigator = () => {
@@ -11,6 +11,9 @@ export const Navigator = () => {
   const handleClick: MouseEventHandler = () => {
     setIsVisible((prev) => !prev);
   };
+  const handleBlur: FocusEventHandler = () => {
+    setIsVisible(false);
+  };
   return (
     <div
       className={cn(
@@ -18,7 +21,12 @@ export const Navigator = () => {
         isVisible && "w-[calc(100%+1rem)] pb-8 backdrop-blur-sm",
       )}
     >
-      <button className="lg:hidden" onClick={handleClick}>
+      <button
+        className="lg:hidden"
+        onBlur={handleBlur}
+        tabIndex={0}
+        onClick={handleClick}
+      >
         <DotIcon />
       </button>
       <nav
@@ -28,11 +36,7 @@ export const Navigator = () => {
         )}
       >
         {Object.values(navigationLinks).map((navInfo) => (
-          <NavigationLink
-            key={navInfo.name}
-            {...navInfo}
-            onClick={handleClick}
-          />
+          <NavigationLink key={navInfo.name} {...navInfo} />
         ))}
       </nav>
     </div>
