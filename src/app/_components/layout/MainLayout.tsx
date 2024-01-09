25248564877a8070b020b145689cc51c0f19285a
LEtMeDEv <79841@naver.com>
2024-01-10 07:53:52 +0900

    padding changed

--- src/app/_components/layout/MainLayout.tsx
+++ src/app/_components/layout/MainLayout.tsx
@@ -5,7 +5,7 @@ type TMainLayoutProps = PropsWithChildren;
 export const MainLayout = ({ children }: TMainLayoutProps) => {
   return (
     <div className="h-screen w-screen p-4 text-sm sm:text-base lg:p-8">
-      <div className=" flex h-full w-full flex-col overflow-scroll border border-primary p-4 scrollbar-hide lg:flex-row lg:p-8">
+      <div className=" flex h-full w-full flex-col overflow-scroll border border-primary p-8 scrollbar-hide lg:flex-row">
         <Header />
         <div className="flex w-full flex-[1] items-end justify-end">
           {children}
